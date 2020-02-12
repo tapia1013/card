7734f99feb81b57de4b0e1fe57e1f3496d5722fa
tapia1013 <manbearpig1013@gmail.com>
2020-02-12 14:02:34 -0800

    Updates

---
+++ precache-manifest.7a557580125e0472bf45fcf639bf786a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530f7c3e6b106844e35a2e9cf6582448",
+    "url": "/card/index.html"
+  },
+  {
+    "revision": "64d15fa3c963f0388d64",
+    "url": "/card/static/css/main.4de147f0.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/card/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/card/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d15fa3c963f0388d64",
+    "url": "/card/static/js/main.094e7ade.chunk.js"
+  },
+  {
+    "revision": "e4e43d76061cb5e93643",
+    "url": "/card/static/js/runtime-main.8f6a9f8d.js"
+  }
+]);
